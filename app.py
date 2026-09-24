2d3bfa6b5f9377825eac83c78178257c62fc7d78
lavanyagowda863 <lavanyagowda863@gmail.com>
2026-09-24 20:15:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker Devlop branch Pipeline!"
+    return "Hello students from Docker Devlop branch Pipeline!"
 
 @app.route("/health")
 def health():
