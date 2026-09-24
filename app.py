0db77072603af2e234c6fead68d5511f803abd72
root <root@ip-172-31-37-97.ap-south-1.compute.internal>
2026-09-24 14:25:05 +0000

    This is from feature

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker Multibranch Pipeline!"
+    return "Hello from Docker feature branch Pipeline!"
 
 @app.route("/health")
 def health():
